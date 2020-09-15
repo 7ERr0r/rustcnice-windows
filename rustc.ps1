82142c7751f08c37e94bebde1610f903ec88f379
Szperak <Szperak@users.noreply.github.com>
2020-09-16 00:11:55 +0200

    rls

--- rustc.ps1
+++ rustc.ps1
@@ -1,4 +1,8 @@
 do{
-Get-WmiObject Win32_process -filter 'name = "rustc.exe"' | foreach-object { $_.SetPriority(64) } | Out-Null
-sleep 1
+	try {
+		Get-WmiObject Win32_process -filter 'name = "rustc.exe" or name = "rls.exe"' | foreach-object { $_.SetPriority(64) } | Out-Null
+	}
+	catch {}
+
+	sleep 1
 }while(1)
